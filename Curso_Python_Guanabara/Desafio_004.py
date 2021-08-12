6265227ba17ceaf654103648b46cebfc63f04b2e
Antônio Marcos <77894621+anttonimarco@users.noreply.github.com>
2021-08-12 15:14:07 -0300

    Type_Program

--- Curso_Python_Guanabara/Desafio_004.py
+++ Curso_Python_Guanabara/Desafio_004.py
@@ -5,45 +5,59 @@
 import time
 
 print('\n')
-print('=' * 60)
-print('<< Type Program >>')
+time.sleep(1)
+print('\033[1;36m*\033[m' * 60)
+time.sleep(1)
+print('\033[1;43m\033[3;30m<<< Type Program >>>\033[m\033[m')
 print('\n')
 
-dado = input('Digite qualquer dado para visualizar suas informações \n')
+time.sleep(1)
+dado = input('\033[1;33mDigite algo:\033[m ')
 print('\n')
 
-print('Tipo Primitivo')
+time.sleep(1)
+print('\033[1;33mTipo Primitivo\033[m')
 print(type(dado))
 print('\n')
 
-print('É Alfabetico?')
+time.sleep(1)
+print('\033[1;33mÉ um número?\033[m')
+print(dado.isnumeric())
+print('\n')
+
+time.sleep(1)
+print('\033[1;33mÉ Alfabetico?\033[m')
 print(dado.isalpha())
 print('\n')
 
-print('É Alfanumérico?')
+time.sleep(1)
+print('\033[1;33mÉ Alfanumérico?\033[m')
 print(dado.isalnum())
 print('\n')
 
-print('Só tem Espaços?')
+time.sleep(1)
+print('\033[1;33mSó tem Espaços?\033[m')
 print(dado.isspace())
 print('\n')
 
-print('Está em Maiúsculo?')
+time.sleep(1)
+print('\033[1;33mEstá em Maiúsculo?\033[m')
 print(dado.isupper())
 print('\n')
 
-print('Está em Minúsculo?')
+time.sleep(1)
+print('\033[1;33mEstá em Minúsculo?\033[m')
 print(dado.islower())
 print('\n')
 
-print('Está Capitalizada?')
+time.sleep(1)
+print('\033[1;33mEstá Capitalizada?\033[m')
 print(dado.istitle())
 print('\n')
 
 
-print('É um número?')
-print(dado.isnumeric())
-print('=' * 60)
+time.sleep(1)
+print('\033[1;36m*\033[m' * 60)
 print('\n')
 
 
