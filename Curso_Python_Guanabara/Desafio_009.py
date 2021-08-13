265af6fd78644888bee7b53259a4dc4e1776d783
Antônio Marcos <77894621+anttonimarco@users.noreply.github.com>
2021-08-13 11:23:10 -0300

    Tabuada

---
+++ Curso_Python_Guanabara/Desafio_009.py
@@ -0,0 +1,27 @@
+"""
+    Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+"""
+
+
+print('\n')
+print('\033[1;33m*\033[m' *40)
+print('\n')
+print('\033[1;31m            >>> Tabuada <<<\033[m')
+print('\n')
+
+numero = int(input('\033[1;33mDigite um número: \033[m'))
+print('\n')
+
+print('\033[1;32m{} x 1 =  {}\033[m'.format(numero, numero * 1))
+print('\033[1;32m{} x 2 =  {}\033[m'.format(numero, numero * 2))
+print('\033[1;32m{} x 3 =  {}\033[m'.format(numero, numero * 3))
+print('\033[1;32m{} x 4 =  {}\033[m'.format(numero, numero * 4))
+print('\033[1;32m{} x 5 =  {}\033[m'.format(numero, numero * 5))
+print('\033[1;32m{} x 6 =  {}\033[m'.format(numero, numero * 6))
+print('\033[1;32m{} x 7 =  {}\033[m'.format(numero, numero * 7))
+print('\033[1;32m{} x 8 =  {}\033[m'.format(numero, numero * 8))
+print('\033[1;32m{} x 9 =  {}\033[m'.format(numero, numero * 9))
+print('\033[1;32m{} x 10 = {}\033[m'.format(numero, numero * 10))
+
+print('\n')
+print('\033[1;33m*\033[m' *40)
